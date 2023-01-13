bc19dd6fc67c18c67c4cc9bfd9e021e87cb4653c
Ivan Letov <letov88@gmail.com>
2023-01-13 23:39:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ gravatar = Gravatar(app,
 
 ##CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://posts_nqft_user:DItdqoyWJ3HavFaRsPWJbPVPONzYx3eh@dpg-cf0rj8ha6gdm8jq1jgdg-a/posts_nqft'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://posts_nqft_user:DItdqoyWJ3HavFaRsPWJbPVPONzYx3eh@dpg-cf0rj8ha6gdm8jq1jgdg-a/posts_nqft'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
