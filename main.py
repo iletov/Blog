fb7e6fd3f84f59da63b73ef2894c256098c55590
Ivan Letov <letov88@gmail.com>
2023-01-14 14:01:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ class Comment(db.Model):
     text = db.Column(db.Text, nullable=False)
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
 def admin_only(function):
